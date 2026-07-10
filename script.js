fdb5f608e26ce2a64c19cc56c6083b49cd26690f
pandeysamriddha3-png <pandeysamriddha3@gmail.com>
2026-07-10 17:54:39 +0530

    Refactor getWeather function in script.js
    Refactor getWeather function for cleaner code and improved readability.

--- script.js
+++ script.js
@@ -1,62 +1,53 @@
 const API_KEY = "81e71249fca24b914673fed677d36512";
 
 
-async function getWeather() {
+async function getWeather(){
 
     let city = document.getElementById("city").value;
 
+    let url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-
-    try {
+    try{
 
         let response = await fetch(url);
 
         let weather = await response.json();
 
+        console.log(weather);
 
-        if (weather.cod == 200) {
 
+        if(weather.cod == 200){
 
             document.getElementById("name").innerHTML =
             "📍 " + weather.name;
 
 
             document.getElementById("temmp").innerHTML =
-            `
-            🌡 Temperature: ${weather.main.temp} °C <br>
-            🤔 Feels like: ${weather.main.feels_like} °C <br>
-            💧 Humidity: ${weather.main.humidity}%
-            `;
+            "🌡 Temp: " + weather.main.temp + " °C";
 
 
             document.getElementById("weatherr").innerHTML =
-            `
-            ☁ Weather: ${weather.weather[0].description}<br>
-            💨 Wind: ${weather.wind.speed} m/s
-            `;
-
+            "☁ " + weather.weather[0].description;
 
         }
 
-        else {
+        else{
 
             document.getElementById("name").innerHTML =
             "❌ " + weather.message;
 
-            document.getElementById("temmp").innerHTML = "";
-
-            document.getElementById("weatherr").innerHTML = "";
         }
 
     }
 
-
     catch(error){
 
         document.getElementById("name").innerHTML =
-        "Something went wrong 😭";
+        error;
+
+        console.log(error);
 
     }
 
