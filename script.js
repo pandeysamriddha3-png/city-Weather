6fdb0856ca12d835ea5e3ee9e12f1c138da044d4
pandeysamriddha3-png <pandeysamriddha3@gmail.com>
2026-07-10 17:51:46 +0530

    Improve weather data display and error messages
    Refactor weather display logic and error handling.

--- script.js
+++ script.js
@@ -1,34 +1,63 @@
 const API_KEY = "81e71249fca24b914673fed677d36512";
 
+
 async function getWeather() {
+
     let city = document.getElementById("city").value;
+
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
+
     try {
+
         let response = await fetch(url);
+
         let weather = await response.json();
+
 
         if (weather.cod == 200) {
 
-            document.getElementById("result").innerHTML = `
-                <hr>
-                <h2>${weather.name}</h2>
-                🌡 Temperature: ${weather.main.temp} °C <br>
-                🤔 Feels like: ${weather.main.feels_like} °C <br>
-                💧 Humidity: ${weather.main.humidity}% <br>
-                ☁ Weather: ${weather.weather[0].description} <br>
-                💨 Wind Speed: ${weather.wind.speed} m/s
-                <hr>
+
+            document.getElementById("name").innerHTML =
+            "📍 " + weather.name;
+
+
+            document.getElementById("temmp").innerHTML =
+            `
+            🌡 Temperature: ${weather.main.temp} °C <br>
+            🤔 Feels like: ${weather.main.feels_like} °C <br>
+            💧 Humidity: ${weather.main.humidity}%
             `;
 
-        } else {
-            document.getElementById("result").innerHTML =
-            "Error: " + weather.message;
+
+            document.getElementById("weatherr").innerHTML =
+            `
+            ☁ Weather: ${weather.weather[0].description}<br>
+            💨 Wind: ${weather.wind.speed} m/s
+            `;
+
+
         }
 
-    } catch(error) {
-        document.getElementById("result").innerHTML =
-        "Something went wrong";
+        else {
+
+            document.getElementById("name").innerHTML =
+            "❌ " + weather.message;
+
+            document.getElementById("temmp").innerHTML = "";
+
+            document.getElementById("weatherr").innerHTML = "";
+        }
+
     }
+
+
+    catch(error){
+
+        document.getElementById("name").innerHTML =
+        "Something went wrong 😭";
+
+    }
+
 }
